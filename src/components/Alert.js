9c1eaedc388639ca0752e52804072e119c6796a2
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-18 11:41:58 +0530

    added more height

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Alert = (props) => {
-    return <div style={{ height: '50px' }} >
+    return <div style={{ height: '80px' }} >
 
             {props.alert && <div className={`alert  alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 {props.alert.message}
